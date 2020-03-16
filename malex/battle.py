42c768d2fcee443ccdf2350949e6062f5c2b69aa
Alex <rfarrel2@uvm.edu>
2020-03-16 11:46:53 -0400

    Refactored battle class and began implementing tests

--- malex/battle.py
+++ malex/battle.py
@@ -8,13 +8,14 @@ from random import randint
 class Battle:
 
     def __init__(self, challenger, defender):
-        self.SIM_DIVIDER = 5
-        self.challenger  = deepcopy(challenger)
-        self.defender    = deepcopy(defender)
-        self.c_stats     = None
-        self.d_stats     = None
-        self.battle_won  = None
-        self.mvp_name    = None
+        self.SIM_CONST  = 5
+        self.challenger = deepcopy(challenger)
+        self.defender   = deepcopy(defender)
+        self.c_stats    = None
+        self.d_stats    = None
+        self.winner     = None
+        self.loser      = None
+        self.mvp_name   = None
 
     def battle(self):
         """
@@ -31,58 +32,52 @@ class Battle:
         self.d_stats = self.defender.get_team_stat_groups()
 
         for key in self.c_stats:
-            self.c_stats[key] *= (randint(1, 5) / self.SIM_DIVIDER)
-            self.d_stats[key] *= (randint(1, 5) / self.SIM_DIVIDER)
+            self.c_stats[key] *= (randint(1, 5) / self.SIM_CONST)
+            self.d_stats[key] *= (randint(1, 5) / self.SIM_CONST)
 
-    def compare(self):
+    def compare_stats(self):
         """
             Compare the simulated stat groups of each trainer to determine
             the winner of the battle.
+        """
+        c_points, d_points = self.add_points()
+
+        if c_points > d_points:
+            self.winner = self.challenger
+            self.loser  = self.defender
+        if c_points < d_points:
+            self.winner = self.defender
+            self.loser  = self.challenger
+
+    def add_points(self):
+        """
+            Add points to trainer with higher simulated stat groups.
         """
         c_points = 0
         d_points = 0
 
         for key in self.c_stats:
-            if self.c_stats[key] > self.d_stats[key]:
-                c_points += 1
-            elif self.c_stats[key] < self.d_stats[key]:
-                d_points += 1
-            else:
-                print('Tie group')
+            c_points += 1 if self.c_stats[key] > self.d_stats[key] else 0
+            d_points += 1 if self.c_stats[key] < self.d_stats[key] else 0
 
-        if c_points > d_points:
-            self.battle_won = True
-        elif c_points < d_points:
-            self.battle_won = False
-        else:
-            print('Tie battle')
+        return c_points, d_points
 
-    def mvp(self):
+    def determine_mvp(self):
         """
             Determine the most valuable pokemon for the trainer who won.
         """
-        if self.battle_won:
-            self.mvp_name = self.challenger.calculate_team_mvp()
-        if not self.battle_won:
-            self.mvp_name = self.defender.calculate_team_mvp()
+        self.mvp_name = self.winner.calculate_team_mvp()
 
-    def display(self):
+    def display_results(self):
         """
             Display the results to the console.
         """
-        if self.battle_won:
-            print('{} defeated {}! {} was the mvp'.format(
-                self.challenger.get_name(), self.defender.get_name(),
-                self.mvp_name
-            ))
-        if not self.battle_won:
-            print('{} defeated {}! {} was the mvp'.format(
-                self.defender.get_name(), self.challenger.get_name(),
-                self.mvp_name
-            ))
+        print('{} defeated {}! {} was the mvp'.format(
+            self.winner.get_name(), self.loser.get_name(), self.mvp_name)
+        )
 
-    def get_result(self):
+    def get_winner(self):
         """
-            Return the result of the battle.
+            Return name of winner of the battle.
         """
-        return self.battle_won
+        return self.winner.get_name()
--- tests/test_battle.py
+++ tests/test_battle.py
@@ -4,21 +4,37 @@ from random import randint
 import unittest
 
 
+class TestingTrainer:
+    ## Fake trainer class for testing                      ##
+
+    def __init__(self, phys, att, dfn):
+        self.team_stat_groups = {
+            'Physical': phys, 'Attack': att, 'Defense': dfn
+        }
+
+    def get_team_stat_groups(self):
+        return self.team_stat_groups
+
+
 class TestBattle(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        print('setUpClass()')
+        #print('setUpClass()')
+        pass
 
     @classmethod
     def tearDownClass(cls):
-        print('tearDownClass()')
+        #print('tearDownClass()')
+        pass
 
     def setUp(self):
-        print('setUp()')
+        #print('setUp()')
+        pass
 
     def tearDown(self):
-        print('tearDown()')
+        #print('tearDown()')
+        pass
 
     #------------------------------------------------------------
 
@@ -26,6 +42,41 @@ class TestBattle(unittest.TestCase):
         ## Ensure simulation is randomized and the stats   ##
         ## change                                          ##
         pass
+
+        """
+        # Generate values and run multiple tests for each
+        # to ensure the stats are changing
+        for sample in range(1):
+            changed = False
+            ch_phys = randint(1, 200)
+            ch_att  = randint(1, 200)
+            ch_dfn  = randint(1, 200)
+
+            d_phys  = randint(1, 200)
+            d_att   = randint(1, 200)
+            d_dfn   = randint(1, 200)
+
+            for test in range(2):
+                challenger  = TestingTrainer(ch_phys, ch_att, ch_dfn)
+                defender    = TestingTrainer(d_phys, d_att, d_dfn)
+                test_battle = Battle(challenger, defender)
+
+                test_battle.simulate()
+                c_stats     = test_battle.c_stats
+                d_stats     = test_battle.d_stats
+
+                for key in c_stats:
+                    if c_stats[key] > challenger.team_stat_groups[key]:
+                        changed = True
+                    if c_stats[key] < challenger.team_stat_groups[key]:
+                        changed = True
+                    if d_stats[key] > defender.team_stat_groups[key]:
+                        changed = True
+                    if d_stats[key] < defender.team_stat_groups[key]:
+                        changed = True
+
+            self.assertTrue(changed)
+        """
 
     def test_compare(self):
         ## Ensure comparison gives points to the trainer   ##
@@ -41,13 +92,6 @@ class TestBattle(unittest.TestCase):
     def test_display(self):
         ## Ensure displays the winner of the battle and    ##
         ## their most valuable pokemon                     ##
-        pass
-
-    #------------------------------------------------------------
-
-    def test_battle(self):
-        ## Ensure battle results are different with the    ##
-        ## same stats.
         pass
 
     #------------------------------------------------------------
