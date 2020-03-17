958d955aaabae42e7ec490875d63e6c8c1e9c006
mario vega <mavega@uvm.edu>
2020-03-17 12:53:38 -0400

    updated the pokemon class to private attributes . added setters and getters with proper syntax.

--- malex/pokemon.py
+++ malex/pokemon.py
@@ -2,19 +2,32 @@
 
 class Pokemon:
     # init method to set the objects attributes
-    def __init__(self):
-        self.__max_level = self
-        self.__name = self
-        self.__level = self
-        self.__stat_groups = self
-        self.__total_stat = self
+    def __init__(self, max_level, name, level, stat_groups, total_stat):
+        self.__max_level = max_level
+        self.__name = name
+        self.__level = level
+        self.__stat_groups = stat_groups
+        self.__total_stat = total_stat
+
+    # setters
+    def set_max_level(self, max_level):
+        self.__max_level = max_level
+
+    def set_name(self, name):
+        self.__name = name
+
+    def set_level(self, level):
+        self.__level = level
+
+    def set_stat_groups(self, stat_groups):
+        self.__stat_groups = stat_groups
+
+    def set_total_stats(self, total_stats):
+        self.__total_stat = total_stats
 
     # getters
     def get_max_level(self):
-        if self.__max_level > 100:
-            print("Max level has been reached !")
-        else:
-            return self.__max_level
+        return self.__max_level
 
     def get_name(self):
         return self.__name
