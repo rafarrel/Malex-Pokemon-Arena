62d82b29d0c9d8fbf814ec267373680279b72718
mario vega <mavega@uvm.edu>
2020-03-19 16:09:39 -0400

    trainer class implementation with more functions .

--- malex/trainer.py
+++ malex/trainer.py
@@ -1,15 +1,19 @@
 # create the trainer class with attributes of max_skill, mvp_variability , max_pokemon , name , skill_level pokemon ,
 # and team_stat_groups
+from malex import pokemon
+import random
+
+
 class Trainer:
     def __init__(self, name, skill_level, pokemon):
-        self.MAX_SKILL       = 10
+        self.MAX_SKILL = 10
         self.MVP_VARIABILITY = 10
-        self.MAX_POKEMON     = 6
-        self.name            = name
-        self.skill_level     = skill_level
-        self.team            = []
+        self.MAX_POKEMON = 6
+        self.name = name
+        self.skill_level = skill_level
+        self.team = []
         self.team_stat_group = {}
-        self.pokemon         = pokemon
+        self.pokemon = pokemon
 
     def add_pokemon(self, pokemon):
         if len(self.team) < self.MAX_POKEMON:
@@ -18,20 +22,24 @@ class Trainer:
             print("pokemon can't be added.")
 
     def calculate_team_stat_groups(self):
-        total_physical                    = 0
-        total_attack                      = 0
-        total_defense                     = 0
-        for key in self.team_stat_group:
-            self.team_stat_group['physical'] += total_physical
-        for key in self.team_stat_group:
-            self.team_stat_group['Attack']   += total_attack
-        for key in self.team_stat_group:
-            self.team_stat_group['Defense']  += total_defense
+        for x in self.team:
+            for key in self.pokemon.stat_groups:
+                self.team_stat_group[key] += self.pokemon.stat_groups[key]
 
     def stat_group_divisor(self):
         for key in self.team_stat_group:
-            self.team_stat_group[key] /= (self.MAX_SKILL - self.skill_level)+1
+            self.team_stat_group[key] /= (self.MAX_SKILL - self.skill_level + 1)
 
+    def get_calculate_team_mvp(self):
+        max_num = 0
+        index   = 0
+        for x in self.team:
+            r1 = random.randint(1, 100)
+            new_total_stat = self.pokemon.get_total_stat()*(r1/self.MVP_VARIABILITY)
+            if max_num < new_total_stat:
+                max_num = new_total_stat
+                index += 1
+        return self.team[index]
 
     def get_name(self):
         return self.name
