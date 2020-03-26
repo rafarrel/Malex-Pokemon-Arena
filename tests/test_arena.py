479ac50258827dc4cd9c373ae11a70dda21b5076
Alex <rfarrel2@uvm.edu>
2020-03-26 16:22:20 -0400

    Finished initial tests for battle and arena. Going to work on refactoring

--- tests/test_arena.py
+++ tests/test_arena.py
@@ -1,51 +1,103 @@
-from malex.arena import Arena
+from malex.arena   import Arena
+from malex.battle  import Battle
+from malex.trainer import Trainer
 from unittest.mock import Mock
 import unittest
+import io
+import sys
 
 
-class TestBattle(unittest.TestCase):
+class TestArena(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.arena = Arena('The Pwn Zone')
-        
+        cls.alex            = Mock(Trainer)
+        cls.mario           = Mock(Trainer)
+        cls.jhibby          = Mock(Trainer)
+        cls.jeff_pesos      = Mock(Trainer)
+        cls.bill_negates    = Mock(Trainer)
+        cls.elon_memelord   = Mock(Trainer)
+        cls.battle          = Battle
 
     def setUp(self):
+        self.TRAINERS = [
+                            self.alex, self.mario, self.jhibby, self.jeff_pesos,
+                            self.bill_negates, self.elon_memelord
+                        ]
+
+        self.arena = Arena('The Pwn Zone')
 
     # ------------------------------------------------------------
 
     def test_run_arena(self):
-        ## Ensure battle_defender called the expected amount ##
-        ## of times.                                         ##
-        print('--------------------------------------------------')
-        EXPECTED_1 = 1
-        EXPECTED_2 = 6
+        ## Ensure battle_defender called an amount of times  ##
+        ## equal to the number of defenders in the arena.    ##
 
+        for trainer in self.TRAINERS:
+            self.arena.battle_defender = Mock()
+            self.arena.defenders.append(trainer)
+            self.arena.run_arena()
+            self.assertEqual(len(self.arena.defenders), self.arena.battle_defender.call_count)
 
     def test_set_challenger(self):
         ## Ensure set challenger references same object as   ##
         ## original challenger.                              ##
-        pass
+        for trainer in self.TRAINERS:
+            self.arena.set_challenger(trainer)
+            self.assertIs(self.arena.challenger, trainer)
 
     def test_add_defender(self):
-        ## Ensure the length of the list of defenders grows  ##
-        ## as defenders are added and that each defender     ##
-        ## references the same objects as the original       ##
+        ## Ensure each defender is added to the list of      ##
         ## defenders.                                        ##
-        pass
+        for trainer in self.TRAINERS:
+            self.arena.add_defender(trainer)
+            self.assertIn(trainer, self.arena.defenders)
 
     def test_battle_defender(self):
         ## Ensure arena_conquered is False and arena is      ##
         ## exited if the challenger loses.                   ##
-        pass
+        self.arena.exit_arena = Mock()
+        self.battle.battle    = Mock(return_value='JHibby')
+        self.alex.get_name    = Mock(return_value='Alex')
+        self.arena.challenger = self.alex
+
+        self.arena.battle_defender(self.jhibby)
+        self.assertFalse(self.arena.arena_conquered)
+        self.assertTrue(self.arena.exit_arena.called)
 
     def test_exit_arena(self):
-        ## Ensure sys.exit() is called.                      ##
-        pass
+        ## Ensure sys.exit and display_results are called    ##
+        with self.assertRaises(SystemExit):
+            self.arena.display_results = Mock()
+            self.arena.exit_arena()
+            self.assertTrue(self.arena.display_results.called)
 
     def test_display_results(self):
         ## Ensure correct message is displayed.              ##
-        pass
+        output      = io.StringIO()
+        sys.stdout  = output
+        exp_display = 'Mario test The Pwn Zone.'
+
+        self.arena.challenger        = self.mario
+        self.mario.get_name          = Mock(return_value='Mario')
+        self.arena.determine_results = Mock(return_value='test')
+        self.arena.display_results()
+        self.assertEqual(exp_display, output.getvalue().rstrip('\n'))
+
+    def test_determine_results(self):
+        ## Ensure correct result is returned                 ##
+
+        # Conquered
+        self.arena.arena_conquered = True
+        result     = self.arena.determine_results()
+        exp_result = 'conquered'
+        self.assertEqual(result, exp_result)
+
+        # Did not conquer
+        self.arena.arena_conquered = False
+        result     = self.arena.determine_results()
+        exp_result = 'did not conquer'
+        self.assertEqual(result, exp_result)
 
 
 if __name__ == '__main__':
